4195b31c23c6a0d4dfcd1fa77c824d80d3ae97c4
rafasousa <140452345+rafasousa1@users.noreply.github.com>
2025-02-11 17:09:36 -0300

    retornando respostas com métodos http diferentes e urls iguais

--- src/server.js
+++ src/server.js
@@ -4,7 +4,17 @@ import http from 'node:http' // O padrão de importações ESMODULES hoje em dia
 // res -> São as respostas que o servidor irá devolver para o usuário
 
 const server = http.createServer((req, res) => { // Criando meu servidor HTTP com uma arrow function
-    return res.end('Hello, Node!')
+    const { method, url } = req // essas duas sendo métodos de REQUISIÇÃO
+
+    if(method === 'GET' && url === '/users') { // Posso ter métodos HTTP diferentes mas com urls iguais
+        return res.end('Lista de usuários') 
+    }
+
+    if(method === 'POST' && url === '/users') {
+        return res.end('Criação de usuários') // Cada método com seu resultado
+    }
+
+    return res.end('Hello, World!')
 })
 
 server.listen(3333) // Fazer com que o server escute na porta 3333 no localhost
