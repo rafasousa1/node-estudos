c7eb4d154b2a2481952dd7caac376e15a75fc768
rafasousa <140452345+rafasousa1@users.noreply.github.com>
2025-02-14 17:33:21 -0300

    entendendo o conceito sobre streams

---
+++ streams/fundamentals.js
@@ -0,0 +1,5 @@
+// Quando pensamos em 'streams' a primeira coisa que vem na cabeça é Netfix, Spotify etc
+// O conceito de streams é utilizado para uploads de arquivos, quando lemos dados que vem da requisição HTTP aos poucos, e processando eles enquanto roda o upload.
+
+// Writalble Streams -> São streams que aos poucos vão mandando informações para o front-end Netfix & Spotify
+// Redable Strams -> São streams que precisamos ler o arquivo aos poucos
