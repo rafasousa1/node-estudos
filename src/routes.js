e133c4eb3113f00d90bbae260d49bde7ff069e7a
rafasousa <140452345+rafasousa1@users.noreply.github.com>
2025-03-07 18:24:08 -0300

    feat: adicionando o método delete nas rotas

--- src/routes.js
+++ src/routes.js
@@ -28,5 +28,12 @@ export const routes = [ // Array de cada rota com o método, o caminho e o que v
 
         return res.writeHead(201).end() // o 201 está simbolizando como OK, com sucesso na criação de algo
         }
+    },
+    {
+        method: 'DELETE',
+        url: '/users/ID', // com o DELETE eu uso um route parameter na URL para identificar um usuário específico a partir do ID
+        handler: (req, res) => {
+            return res.end()
+        }
     }
 ]
